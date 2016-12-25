137edc8449a0f1b2559a719d369493f9e5feb2b7
Tarik <tarik.pribisic@stu.ibu.edu.ba>
2016-12-25 23:07:41 +0100

    Google Maps API and Gridview + icons implemented.

---
+++ app/src/main/java/com/example/lejla/gosarajevo/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.example.lejla.gosarajevo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lejla on 12/23/2016.
+ */
+
+@SuppressWarnings("ALL")
+public class AboutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/lejla/gosarajevo/ImageAdapter.java
@@ -0,0 +1,67 @@
+package com.example.lejla.gosarajevo;
+
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(0, 0, 0, 0);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.ic_bar,
+                R.drawable.ic_shopping,
+                R.drawable.ic_cinema,
+                R.drawable.ic_play,
+                R.drawable.ic_restaurant,
+                R.drawable.ic_cafe,
+                R.drawable.ic_hotel,
+                R.drawable.ic_hospital,
+                R.drawable.ic_gas,
+                R.drawable.ic_taxi,
+                R.drawable.ic_bus,
+                R.drawable.ic_airport,
+                R.drawable.ic_atm,
+                R.drawable.ic_account_balance_black_24dp,
+                R.drawable.ic_icon,
+                R.drawable.ic_university,
+        };
+    }
